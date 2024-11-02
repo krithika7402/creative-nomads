438267d55a4fdf521e887bba418397dec2421f80
Krithika N <krithikan@presidio.com>
2024-11-03 03:54:37 +0530

    loader and animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 // App.jsx
+import { useState, useEffect } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import PageLoader from "./components/PageLoader/PageLoader";
 
-// App.tsx
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      setTimeout(() => {
+        setShowContent(true);
+      }, 100);
+    }, 2250);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
   return (
-    <div className="app-container">
+    <div className={`app-container ${showContent ? 'fade-in' : ''}`}>
       <Header />
       <div className="app-content">
         <Sidebar />
@@ -18,4 +37,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
